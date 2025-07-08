471f2f336c89b7fcfd3293d3321716e3e7cc351a
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2025-07-08 20:32:41 +0200

    configure code coverage

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -12,4 +12,20 @@ module.exports = {
   transform: {
     ...tsJestTransformCfg,
   },
+  collectCoverage: false,
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/**/*.d.ts",
+    "!src/index.ts"
+  ],
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov", "html"],
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50
+    }
+  }
 };
